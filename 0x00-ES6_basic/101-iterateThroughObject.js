be43daf9b8d37add96b4551a7e610de105eaa86f
Jany Muong <me.roumuong@gmail.com>
2023-09-30 17:18:06 +0300

    iterate through object

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,10 @@
+export default function iterateThroughObject(reportWithIterator) {
+  let employeeNames = '';
+
+  // iterate over the report iterator;
+  for (const employee of reportWithIterator) {
+    employeeNames += `${employee}|`;
+  }
+
+  return employeeNames;
+}
