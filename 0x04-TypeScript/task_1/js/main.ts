2ac667978c1ba8233b07d2303e43d576befac79f
Jany Muong <me.roumuong@gmail.com>
2023-10-07 18:09:50 +0300

    class for printing teachers; interface of it

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,34 +7,44 @@ interface Teacher {
     [key: string]: any;
   }
 
-  const teacher1: Teacher = {
-    firstName: 'GNU Linux Person',
-    fullTimeEmployee: true,
-    lastName: 'OS',
-    location: 'Nairobi, Kenya',
-  };
+//   const teacher1: Teacher = {
+//     firstName: 'GNU Linux Person',
+//     fullTimeEmployee: true,
+//     lastName: 'OS',
+//     location: 'Nairobi, Kenya',
+//   };
 
-  const teacher3: Teacher = {
-    firstName: 'Doe',
-    fullTimeEmployee: false,
-    lastName: 'Smith',
-    location: 'Birmingham',
-    contract: false,
-  };
+//   const teacher3: Teacher = {
+//     firstName: 'Doe',
+//     fullTimeEmployee: false,
+//     lastName: 'Smith',
+//     location: 'Birmingham',
+//     contract: false,
+//   };
 
 //   console.log(teacher1);
 //   console.log(teacher3);
 
-  interface Directors extends Teacher {
+interface Directors extends Teacher {
     numberOfReports: number;
   }
 
-  const director1: Directors = {
-    firstName: 'Erwin',
-    lastName: 'Smith',
-    location: 'Paradis Island',
-    fullTimeEmployee: true,
-    numberOfReports: 17,
-  };
+//   const director1: Directors = {
+//     firstName: 'Erwin',
+//     lastName: 'Smith',
+//     location: 'Paradis Island',
+//     fullTimeEmployee: true,
+//     numberOfReports: 17,
+//   };
 
-  console.log(director1);
+//   console.log(director1);
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+  }
+
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
+
+console.log(printTeacher("GNU-Linux", "Person")); // logs 'G. Person'
