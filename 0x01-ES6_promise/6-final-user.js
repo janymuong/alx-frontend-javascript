d2c123386af783519b13c2d4f9d8320cdaa9a054
Jany Muong <me.roumuong@gmail.com>
2023-10-20 18:20:35 +0300

    handle promises - others `pending`

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,8 +6,11 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const photoPromise = uploadPhoto(fileName);
 
   return Promise.allSettled([userPromise, photoPromise])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+    .then((results) => {
+      const arr = [];
+      for (const item of results) {
+        arr.push({ status: item.status, value: item.value || item.reason });
+      }
+      return arr;
+    });
 }
